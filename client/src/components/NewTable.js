c0e1de9bdd553e1b116147fee7a9f5bc27d6d5ba
Dana Cronin <cronin43224235@gmail.com>
2020-06-06 11:21:59 -0500

    psudoing needs baseling

---
+++ client/src/components/NewTable.js
@@ -0,0 +1,9 @@
+// model-style pop up 
+
+// form & inputs
+    // table name/title
+    // desc
+    // columns (list style until 'created')
+    //     (auto Title {type: text})
+    //     add column btn
+
